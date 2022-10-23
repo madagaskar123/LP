a874779fd26ac01a7fe0232d5fc029b483d9ace9
madagaskar123 <115578759+madagaskar123@users.noreply.github.com>
2022-10-23 17:41:33 +0300

    Add files via upload

--- HomeWorks/HomeWork3/Program.cs
+++ HomeWorks/HomeWork3/Program.cs
@@ -17,7 +17,7 @@ string? number = Console.ReadLine();
 
 void CheckingNumber(string number)
 {
-  if (number[0]==number[4] || number[1]==number[3])
+  if (number[0]==number[4] && number[1]==number[3])
   {
     Console.WriteLine($"Ваше число: {number} - палиндром.");
   }
